a77186dad7d2a9a478d99895986e27813f9b3270
coohjaay <rieke@meissner-online.org>
2026-06-22 11:04:40 +0200

    Bashskript um die wichtigen Datan zu pullen

---
+++ setup_for_data.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if [ ! -d data/BF-Open-Data ]; then
+  git clone --filter=blob:none --no-checkout https://github.com/Berliner-Feuerwehr/BF-Open-Data data/BF-Open-Data
+  cd data/BF-Open-Data
+  git sparse-checkout init --cone
+  git sparse-checkout set Datasets/Daily_Data Datasets/Mission_Data Datasets/Dispatchcodes
+  git checkout main
+else
+  cd data/BF-Open-Data && git pull
+fi
+
